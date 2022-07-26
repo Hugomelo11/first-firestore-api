b2fb7b20cdf3df02ce819c0bdc8bf700e6863006
Hugomelo <salles.hugo@gmail.com>
2022-07-26 16:17:47 -0400

    get all cars;

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { getAllCars } from './src/cars.js';
 
 const app = express();
 const PORT = 3002;
@@ -6,9 +7,11 @@ app.use(express.json());
 
 // put our routes here
 app.get('/', (req, res) => {
-    res.send('Express is workinggggg 😛');
+    res.send('Express is working!');
 });
 
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}/ ...`)
-}); 
+});
+
+app.get('/cars', getAllCars);
